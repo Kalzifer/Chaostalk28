a15805ebe9be00f3badea32ebd521c3f0a20729d
kalzifer <kalzifer@github.com>
2017-06-13 00:07:34 +0200

    Bugfix: overwrite reading var

--- examples/tmp36/src/main.cpp
+++ examples/tmp36/src/main.cpp
@@ -18,9 +18,7 @@ void loop()                     // run over and over again
 {
  //getting the voltage reading from the temperature sensor
  int reading = analogRead(sensorPin);
- reading = 1;
-
-
+ Serial.print(reading); Serial.println(" ADC value");
  // converting that reading to voltage, for 3.3v arduino use 3.3
  float voltage = reading * 3.3;
  voltage /= 1024.0;
@@ -31,12 +29,8 @@ void loop()                     // run over and over again
  // now print out the temperature
  float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
                                                //to degrees ((voltage - 500mV) times 100)
- Serial.print(temperatureC); Serial.println(" degrees C");
-
- // now convert to Fahrenheit
- //float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
- //Serial.print(temperatureF);
- //Serial.println(" degrees F");
-
+ Serial.print(temperatureC);
+ Serial.println(" degrees C");
+ Serial.println("============");
  delay(1000);                                     //waiting a second
 }
